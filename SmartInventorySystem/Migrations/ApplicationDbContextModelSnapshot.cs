9b6e61a209d4585808825433deb3ad9f633a2e21
Ibrahimdur1989 <Ibrahimdur1989@gmail.com>
2025-04-12 22:18:21 -0400

    database2

--- SmartInventorySystem/Migrations/ApplicationDbContextModelSnapshot.cs
+++ SmartInventorySystem/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -101,12 +101,10 @@ namespace SmartInventorySystem.Migrations
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityUserLogin<string>", b =>
                 {
                     b.Property<string>("LoginProvider")
-                        .HasMaxLength(128)
-                        .HasColumnType("character varying(128)");
+                        .HasColumnType("text");
 
                     b.Property<string>("ProviderKey")
-                        .HasMaxLength(128)
-                        .HasColumnType("character varying(128)");
+                        .HasColumnType("text");
 
                     b.Property<string>("ProviderDisplayName")
                         .HasColumnType("text");
@@ -143,12 +141,10 @@ namespace SmartInventorySystem.Migrations
                         .HasColumnType("text");
 
                     b.Property<string>("LoginProvider")
-                        .HasMaxLength(128)
-                        .HasColumnType("character varying(128)");
+                        .HasColumnType("text");
 
                     b.Property<string>("Name")
-                        .HasMaxLength(128)
-                        .HasColumnType("character varying(128)");
+                        .HasColumnType("text");
 
                     b.Property<string>("Value")
                         .HasColumnType("text");
